ff6278cb8caa7310c66d5b97caee74ed9491e9c6
Sean Iveson <seanarooni@gmail.com>
2012-12-03 10:39:27 -0500

    first commit, working client side with buttons

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,37 @@
-/* Author: YOUR NAME HERE
+/* Author: Sean Iveson
 */
 
 $(document).ready(function() {   
 
   var socket = io.connect();
 
+  $('#up').bind('click', function() {
+  	socket.emit('message', 'Yellow button pressed on ' + new Date());
+  	document.panel.src='http://www.tadcoenvironmental.com/simon/upSelected1.png';
+  	setTimeout(function(){ document.panel.src='http://www.tadcoenvironmental.com/simon/SimonS2.png'; }, 200);
+  });
+  
+  $('#right').bind('click', function() {
+  	socket.emit('message', 'Red button pressed on ' + new Date());
+  	document.panel.src='http://www.tadcoenvironmental.com/simon/leftSelected1.png';
+  	setTimeout(function(){ document.panel.src='http://www.tadcoenvironmental.com/simon/SimonS2.png'; }, 200);
+
+  });
+  
+  $('#down').bind('click', function() {
+  	socket.emit('message', 'Blue button pressed on ' + new Date());
+  	document.panel.src='http://www.tadcoenvironmental.com/simon/downSelected1.png';
+  	setTimeout(function(){ document.panel.src='http://www.tadcoenvironmental.com/simon/SimonS2.png'; }, 200);
+
+  });
+  
+  $('#left').bind('click', function() {
+  	socket.emit('message', 'Green button pressed on ' + new Date());
+  	document.panel.src='http://www.tadcoenvironmental.com/simon/rightSelected1.png';
+  	setTimeout(function(){ document.panel.src='http://www.tadcoenvironmental.com/simon/SimonS2.png'; }, 200);
+
+  });
+  
   $('#sender').bind('click', function() {
    socket.emit('message', 'Message Sent on ' + new Date());     
   });
